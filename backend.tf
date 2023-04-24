517d4d2c0f675dbef4e5f1476419625c68d516c6
Hendry Setiawan <hndry.st@gmail.com>
2023-04-24 18:32:12 +0700

    Move comment to readme.md

--- backend.tf
+++ backend.tf
@@ -1,10 +1,3 @@
 terraform {
   backend "gcs" {}
 }
-
-/*
-terraform init -backend-config=backend.hcl
-need backend.hcl file with the bucket name and prefix
-bucket = "bucketname"
-prefix = "dirname/state"
-*/
--- variables.tf
+++ variables.tf
@@ -26,10 +26,3 @@ variable "host_project" {
   type    = string
   default = ""
 }
-/*
-need terraform.tvars file for additional config
-project= "projectId"
-host_project = "hostProjectId"
-vpc = vpc selflink -> gcloud compute networks describe vpcName --format="value(selfLink)"
-subnet = subnet selflink -> gcloud compute networks subnets describe subnetName --format="value(selfLink)" --region=subnetRegion
-*/
